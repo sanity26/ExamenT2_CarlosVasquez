c154d174a11be69ca8760cb3bbba1400f0ef9186
SUITE <CarlosVasquez>
2025-10-14 21:47:43 -0500

    Proyecto iniciado

---
+++ src/main/java/com/examen_t2/spring_crud_jpa/SpringCrudJpaApplication.java
@@ -0,0 +1,13 @@
+package com.examen_t2.spring_crud_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudJpaApplication.class, args);
+	}
+
+}
